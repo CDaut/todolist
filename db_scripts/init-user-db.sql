18b64b91b796ffeef149f0183f18f1db8b564dd9
Clemens-Dautermann <clemens.dautermann@t-online.de>
2021-11-15 18:22:46 +0100

    finished basic docker setup for django and postgres

---
+++ db_scripts/init-user-db.sql
@@ -0,0 +1,7 @@
+\set dbuser `echo "$POSTGRES_USER"`
+\set dbname `echo "$POSTGRES_DB"`
+\set pwd `echo "$POSTGRES_PASSWORD"`
+
+CREATE USER :'dbuser';
+CREATE DATABASE :'dmname';
+GRANT ALL PRIVILEGES ON DATABASE :'dbname' TO :'dbuser';
--- todolist_server/todolist_server/settings.py
+++ todolist_server/todolist_server/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-w66^%r)jpm)k*w^0tu0(8if&5xr&b&7(%gu)2z)d&kry_+c0ij
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,17 +67,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todolist_server.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ["POSTGRES_DB"],
+        'USER': os.environ["POSTGRES_USER"],
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+        'HOST': 'db',
+        'PORT': '5432',
     }
-}
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -99,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -112,7 +112,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
