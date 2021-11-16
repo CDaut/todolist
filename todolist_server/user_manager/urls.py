783a4095a9195467a5fab44be1131e7d4e5ac24e
Clemens-Dautermann <clemens.dautermann@t-online.de>
2021-11-16 22:54:25 +0100

    completed login frontetnd

--- todolist_server/user_manager/urls.py
+++ todolist_server/user_manager/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from user_manager.views import login_view
 
 urlpatterns = [
-    path('login/', login_view)
+    path('login/', login_view, name='login')
 ]
--- todolist_server/user_manager/views.py
+++ todolist_server/user_manager/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def login_view(request):
-    context = {'title': 'Log-in'}
-    return render(request, 'user_manager/login.html', context)
+    if request.method == 'GET':
+        context = {'title': 'Log-in'}
+        return render(request, 'user_manager/login.html', context)
