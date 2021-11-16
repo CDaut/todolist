09c2cc530e9f445cf859f98158abfac4cb2e73fb
Clemens-Dautermann <clemens.dautermann@t-online.de>
2021-11-16 19:03:00 +0100

    finished base template

--- todolist_server/task_display/urls.py
+++ todolist_server/task_display/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from task_display.views import list_tasks_as_list
+from task_display.views import list_tasks_as_list, redirect_to_list_view
 
 urlpatterns = [
     path('tasks/list/', list_tasks_as_list),
+    path('tasks/', redirect_to_list_view),
 ]
--- todolist_server/task_display/views.py
+++ todolist_server/task_display/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required
 def list_tasks_as_list(request):
-    context = {'title': 'Tasklist'}
+    context = {'title': 'Tasklist',
+               'fullname': request.user.first_name + request.user.last_name,
+               'email': request.user.email}
     return render(request, 'task_display/task_list.html', context)
+
+
+def redirect_to_list_view(request):
+    return redirect('list/')
